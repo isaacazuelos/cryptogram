854525c121b87703bd301f32b320a5517f6903b0
Isaac Azuelos <isaac@azuelos.ca>
2016-04-22 15:49:25 -0600

    All solver tests pass

--- src/Toy/Cryptogram/Solver.hs
+++ src/Toy/Cryptogram/Solver.hs
@@ -12,6 +12,7 @@ module Toy.Cryptogram.Solver
     )
   where
 
+import           Data.Maybe                (mapMaybe)
 import           Data.Ord                  (comparing)
 
 import qualified Data.Char                 as Char
@@ -20,12 +21,17 @@ import qualified Data.Text                 as Text
 import qualified Toy.Cryptogram.Dictionary as Dict
 import qualified Toy.Cryptogram.Key        as Key
 
-
 solutions :: Dict.Dictionary -> Text.Text -> [Key.Key]
-solutions dict ct = undefined
+solutions dict ct = go (filter usable (Text.words ct)) Key.empty
+  where
+    go [] k = [k]
+    go (w:ws) key = expandKey dict key w >>= go ws
 
 usable :: Text.Text -> Bool
-usable = undefined
+usable = Text.all Char.isAsciiUpper
 
 expandKey :: Dict.Dictionary -> Key.Key -> Text.Text -> [Key.Key]
-expandKey d k w = undefined
+expandKey d k w =
+  if usable w
+    then mapMaybe (flip (Key.insert k) w) (Dict.lookup d w)
+    else mempty
